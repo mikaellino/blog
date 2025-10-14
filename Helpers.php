9c671495d8fcb5af1f77123687b1d50b2147ca63
mikaellino <mikael.lino123@gmail.com>
2025-10-13 22:25:29 -0300

    feat: Adding new function to count the time

--- Helpers.php
+++ Helpers.php
@@ -2,12 +2,51 @@
 // Arquivo de funções auxiliares
 
 /**
+ * Conta o tempo decorrido a partir de uma data
+ * @param string $data
+ * @return string
+ */
+function contarTempo(string $data): string
+{
+    $agora = strtotime(date('Y-m-d H:i:s'));
+    $tempo = strtotime($data);
+    $diferenca = $agora - $tempo;
+
+    $segundos = $diferenca;
+    $minutos = round($diferenca/60);
+    $horas = round($diferenca/3600);
+    $dias = round($diferenca/86400); 
+    $semanas = round($diferenca/604800);
+    $meses = round($diferenca/2419200);
+    $anos = round($diferenca/29030400);
+
+    if($segundos <= 60) {
+        return 'agora';
+    }elseif($minutos<=60) {
+        return $minutos == 1 ? 'há um minuto' : 'há ' . $minutos . ' minutos';
+    }elseif($horas<=24) {
+        return $horas == 1 ? 'há uma hora' : 'há ' . $horas . ' horas';
+    }elseif($dias<=7) {
+        return $dias == 1 ? 'ontem' : 'há ' . $dias . ' dias';
+    }elseif($semanas<=4) {
+        return $semanas == 1 ? 'há uma semana' : 'há ' . $semanas . ' semanas';
+    }elseif($meses<=12) {
+        return $meses == 1 ? 'há um mês' : 'há ' . $meses . ' meses';
+    }else{
+        return $anos == 1 ? 'há um ano' : 'há ' . $anos . ' anos';
+    }
+
+    echo '<hr>';
+    var_dump($minutos);
+}
+
+/**
  * Formata valores, inserindo . e , de acordo com as casas decimais
  * 
  * @param float $valor opcional - valor de entrada que será formatado
  * @return string valor formatado
  */
-function formatarValor(float $valor = null) : string
+function formatarValor(float $valor = null): string
 {
     return number_format(($valor ? $valor : 0), 2, ',', '.');
 }
@@ -18,9 +57,9 @@ function formatarValor(float $valor = null) : string
  * @param int $numero opcional - recebe o número que será formatado
  * @return string número formatado
  */
-function formatarNumero(int $numero = null) : string
+function formatarNumero(int $numero = null): string
 {
-    return number_format($numero ? $numero : 0,0,'.','.');
+    return number_format($numero ? $numero : 0, 0, '.', '.');
 }
 
 function saudacao(): string
