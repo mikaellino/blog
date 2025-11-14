5be73eb1fe7dbfa4b87f27c3173f0c8690d168bd
Mikael Lino <mikael.lino123@gmail.com>
2025-11-14 18:54:33 -0300

    feat: Template functions.

--- sistema/Controller/ControllerDAO.php
+++ sistema/Controller/ControllerDAO.php
@@ -2,20 +2,37 @@
 
 namespace sistema\Controller;
 
-class ControllerDAO
+use sistema\Nucleo\Controlador;
+
+class ControllerDAO extends Controlador
 {
+
+    public function __construct()
+    {
+        parent::__construct('templates/site/views');
+    }
+
     public function index(): void
     {
-        echo "Página index!";
+        echo $this->template->renderizar('index.html', [
+            'titulo' => 'teste de título',
+            'subtitulo' => 'teste de subtitulo'
+        ]);
     }
 
     public function sobre(): void
     {
-        echo "Página sobre!";
+        echo $this->template->renderizar('sobre.html', [
+            'titulo' => 'Página de sobre',
+            'subtitulo' => 'teste de subtítulo da página sobre'
+        ]);
     }
 
     public function home(): void
     {
-        echo "Página home";
+        echo $this->template->renderizar('home.html', [
+            'titulo' => 'Página Home',
+            'subtitulo' => 'Subtítulo da Página Home'
+        ]);
     }
 }
--- sistema/Nucleo/Controlador.php
+++ sistema/Nucleo/Controlador.php
@@ -2,10 +2,15 @@
 
 namespace sistema\Nucleo;
 
+use sistema\Suporte\Template;
+
 class Controlador
 {
-    public function __construct(string $tema = null)
+    protected Template $template;
+
+
+    public function __construct(string $diretorio)
     {
-        echo $tema;
+        $this->template = new Template($diretorio);
     }
 }
