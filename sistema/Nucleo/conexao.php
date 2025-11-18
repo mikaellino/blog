39fbcbf4e727836102b0e346e51e52510c0e5319
mikaellino <mikael.lino123@gmail.com>
2025-11-18 10:54:04 -0300

    feat/teste: Connection to the BD with PDO implemented.

---
+++ sistema/Nucleo/conexao.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace sistema\Nucleo;
+
+use PDO;
+use PDOException;
+
+class Conexao
+{
+    private static $instancia;
+
+    public static function getInstancia(): PDO
+    {
+        if (empty(self::$instancia)) {
+            try {
+                self::$instancia = new PDO('mysql:host='. DB_HOST .';port='. DB_PORTA .';dbname='. DB_NAME, DB_USER, DB_PASSWORD, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_CASE => PDO::CASE_NATURAL
+                ]);
+            } catch (PDOException $e) {
+                die("Erro de conexão:: " . $e->getMessage());
+            }
+        }
+        return self::$instancia;
+    }
+
+    protected function __construct()
+    {
+
+    }
+
+    private function __clone(): void
+    {
+        
+    }
+}
--- sistema/configuracao.php
+++ sistema/configuracao.php
@@ -4,6 +4,12 @@
 //define o fuso horário padrão do sistema
 date_default_timezone_set('America/Sao_Paulo');
 
+define('DB_HOST', 'localhost');
+define('DB_PORTA', '3306');
+define('DB_NAME', 'blog');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
 //Informações do site
 define('SITE_NOME', 'Unset ');
 define('SITE_DESCRICAO', 'Unset - Tecnologia em Sistemas');
