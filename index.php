e7dc02b96397234dbf3a99a127f843ab99913e2b
mikaellino <mikael.lino123@gmail.com>
2025-11-25 10:57:41 -0300

    refact: tests removed.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 //Arquivo index responsável pela inialização do sistema
+require_once 'vendor/autoload.php';
 
-// require 'rotas.php';
-
-require_once 'sistema/Nucleo/conexao.php';
-require_once 'sistema/configuracao.php';
-
-use sistema\Nucleo\Conexao;
-
-$conn = Conexao::getInstancia();
-
+require 'rotas.php';
