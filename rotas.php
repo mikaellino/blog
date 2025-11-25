338dc0dba9ced065746e47125a47018b49a4bfcc
mikaellino <mikael.lino123@gmail.com>
2025-11-25 10:58:26 -0300

    feat: Add posts rotes.

--- rotas.php
+++ rotas.php
@@ -10,6 +10,8 @@ try {
     SimpleRouter::get(URL_SITE . 'sobre-nos', 'ControllerDAO@sobre');
     SimpleRouter::get(URL_SITE . 'home', 'ControllerDAO@home');
     SimpleRouter::get(URL_SITE . 'myPage', 'ControllerDAO@myPage');
+    SimpleRouter::get(URL_SITE . 'publicacao/{id}', 'ControllerDAO@publicacao');
+
     SimpleRouter::get(URL_SITE . '404', 'ControllerDAO@erro404');
 
     SimpleRouter::start();
